7440d2a146b236cd3b59b9a9ec5f1fc5e09f32bd
DaniilLeonov <baslook23@gmail.com>
2021-10-17 23:46:56 +0300

    added "E"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,23 @@
 #include <array>
 
 using namespace std;
+void e(array<string,6> &s){
+s[0]+=" _____   ";
+s[1]+="|         ";
+s[2]+="|_____  ";
+s[3]+="|         ";
+s[4]+="|         ";
+s[5]+="|_____  ";
 
+
+
+
+
+}
 int main()
 {
     array<string, 6> screen;
-
+    e(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
