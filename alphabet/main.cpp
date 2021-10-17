a16827d233d1aa5b256d936301907f8d6d007be4
Lucky Potato <arepev2006@mail.ru>
2021-10-18 00:20:39 +0300

    Added i

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,19 @@
 #include <array>
 
 using namespace std;
+void i(array<string, 6>& s) {
+    s[0] += "___    ";
+    s[1] += " |     ";
+    s[2] += " |     ";
+    s[3] += " |     ";
+    s[4] += " |     ";
+    s[5] += "_|_    ";
 
+}
 int main()
 {
     array<string, 6> screen;
-
+    i(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
