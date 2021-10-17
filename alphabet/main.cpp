fa74b5ab604717514b9ce4c97e67d9ae937b76e7
DaniilLeonov <baslook23@gmail.com>
2021-10-18 00:03:04 +0300

    added "H"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,19 @@
 #include <array>
 
 using namespace std;
+void h(array<string,6> &s){
+s[0]+="|     |   ";
+s[1]+="|     |   ";
+s[2]+="|_____|   ";
+s[3]+="|     |   ";
+s[4]+="|     |   ";
+s[5]+="|     |   ";
 
+}
 int main()
 {
     array<string, 6> screen;
-
+    h(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
