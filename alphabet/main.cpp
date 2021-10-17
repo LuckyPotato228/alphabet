2861160a3af82135ee49588907fd2a7978e15e24
Lucky Potato <arepev2006@mail.ru>
2021-10-18 00:56:12 +0300

    Added O

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,18 @@
 #include <array>
 
 using namespace std;
-
+void o(array<string, 6>& s) {
+    s[0] += "  _____";
+    s[1] += " /      \\   ";
+    s[2] += "/        \\";
+    s[3] += "|        |";
+    s[4] += "\\       /";
+    s[5] += " \\_____/";
+}
 int main()
 {
     array<string, 6> screen;
-
+    o(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
