b6be8cc70a57c7d09a1a61b5c2decdbf1962bf88
DaniilLeonov <baslook23@gmail.com>
2021-10-18 01:13:01 +0300

    added "W"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,23 @@
 #include <array>
 
 using namespace std;
+void W(array<string,6> &s){
+s[0]+="\\                  /";
+s[1]+=" \\                / ";
+s[2]+="  \\      /\\      /  ";
+s[3]+="   \\    /  \\    /   ";
+s[4]+="    \\  /    \\  /    ";
+s[5]+="     \\/      \\/     ";
 
+
+
+
+
+}
 int main()
 {
     array<string, 6> screen;
-
+    W(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
