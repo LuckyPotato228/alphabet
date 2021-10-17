7b45a1598f0bd5989974f4c2115782f096392493
DaniilLeonov <baslook23@gmail.com>
2021-10-18 00:29:49 +0300

    added "N"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,23 @@
 #include <array>
 
 using namespace std;
+void N(array<string,6> &s){
+s[0]+="||\\\\\    || ";
+s[1]+="|| \\\\   ||  ";
+s[2]+="||  \\\\  ||  ";
+s[3]+="||   \\\\ ||  ";
+s[4]+="||    \\\\||  ";
+s[5]+="||     \\\||   ";
 
+
+
+
+
+}
 int main()
 {
     array<string, 6> screen;
-
+    N(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
