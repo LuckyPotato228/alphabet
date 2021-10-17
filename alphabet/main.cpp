71b0e02ecab3d958dbc75004e70070608a83f078
Lucky Potato <arepev2006@mail.ru>
2021-10-18 00:05:54 +0300

    added F

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,19 @@
 #include <array>
 
 using namespace std;
+void f(array<string, 6>& s) {
+    s[0] += " _____";
+    s[1] += "|";
+    s[2] += "|___";
+    s[3] += "|";
+    s[4] += "|";
+    s[5] += "|";
 
+}
 int main()
 {
     array<string, 6> screen;
-
+    f(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
