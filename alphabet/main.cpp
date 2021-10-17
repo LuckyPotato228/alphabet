7bc1c480a3be3d4d2e03f023127966fa785e2cb3
DaniilLeonov <baslook23@gmail.com>
2021-10-18 00:44:53 +0300

    added "Q"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,11 +2,21 @@
 #include <array>
 
 using namespace std;
+void q(array<string,6> &s){
+s[0]+="  ______    ";
+s[1]+=" /      \\   ";
+s[2]+="|        |  ";
+s[3]+="|        |  ";
+s[4]+=" \\___\\\\_/   ";
+s[5]+="      \\\\    ";
 
+
+
+}
 int main()
 {
     array<string, 6> screen;
-
+    q(screen);
     for (int i=0; i<6; i++)
         cout<< screen[i]<<endl;
 
