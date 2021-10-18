7ff16acf696ab5927cf547606a64a1f612f3c060
Mops <mrmops2005@gmail.com>
2021-10-18 18:28:34 +0300

    Added G

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,13 +2,27 @@
 #include <array>
 
 using namespace std;
+void g(array<string, 6> &s){
+s[0]+= "      ____    " ;
+s[1]+= "    /    \   " ;
+s[2]+= "    |         " ;
+s[3]+= "    | ___   " ;
+s[4]+= "    |    \\ " ;
+s[5]+= "    \\____/  " ;
 
+}
 int main()
 {
+
     array<string, 6> screen;
+    g(screen);
+
+
 
     for (int i=0; i<6; i++)
+
         cout<< screen[i]<<endl;
+
 
     return 0;
 }
