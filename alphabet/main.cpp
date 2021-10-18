1ab9646d468723461050e5e4b01d120e05c715cb
Mops <mrmops2005@gmail.com>
2021-10-18 18:53:42 +0300

    added C

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,13 +2,27 @@
 #include <array>
 
 using namespace std;
+void C(array<string, 6> &s){
+s[0]+= "     _____ " ;
+s[1]+= "    |      " ;
+s[2]+= "    |      " ;
+s[3]+= "    |      " ;
+s[4]+= "    |      " ;
+s[5]+= "    |_____ " ;
 
+}
 int main()
 {
+
     array<string, 6> screen;
+    C(screen);
+
+
 
     for (int i=0; i<6; i++)
+
         cout<< screen[i]<<endl;
+
 
     return 0;
 }
