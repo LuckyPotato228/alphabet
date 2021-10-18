076b650f929224dfc3ad6f7a65de10235142158d
Mops <mrmops2005@gmail.com>
2021-10-18 18:07:47 +0300

    added "Y"

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -1,14 +1,30 @@
+
 #include <iostream>
 #include <array>
 
 using namespace std;
+void y(array<string, 6> &s){
+s[0]+= "\\\\    //" ;
+s[1]+= " \\\\  //  " ;
+s[2]+= "  \\\\//   " ;
+s[3]+= "   || " ;
+s[4]+= "   ||  " ;
+s[5]+= "   ||  " ;
 
+}
 int main()
 {
+
     array<string, 6> screen;
+    y(screen);
+
+
 
     for (int i=0; i<6; i++)
+
         cout<< screen[i]<<endl;
+
 
     return 0;
 }
+
