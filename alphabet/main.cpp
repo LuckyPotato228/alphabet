d1f7d3a9f5b2ae72cdf14eff1e7131cc2b63c63c
Mops <mrmops2005@gmail.com>
2021-10-18 18:48:13 +0300

    added J

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -2,13 +2,27 @@
 #include <array>
 
 using namespace std;
+void J(array<string, 6> &s){
+s[0]+= "   *     " ;
+s[1]+= "   |     " ;
+s[2]+= "   |     " ;
+s[3]+= "   |     " ;
+s[4]+= "   |     " ;
+s[5]+= " /_|     " ;
 
+}
 int main()
 {
+
     array<string, 6> screen;
+    J(screen);
+
+
 
     for (int i=0; i<6; i++)
+
         cout<< screen[i]<<endl;
+
 
     return 0;
 }
